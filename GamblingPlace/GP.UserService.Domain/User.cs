ea4e969eb8d06fe1238c775a08743722408f6e9e
deivid7007 <deivid7007@mail.bg>
2018-01-28 17:13:27 +0200

    Added user model.

---
+++ GamblingPlace/GP.UserService.Domain/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GP.UserService.Domain
+{
+    public sealed class User
+    {
+        public User()
+        {
+            
+        }
+        public User(string email, string password,string id = null)
+        {
+            this.Email = email;
+            this.Password = password;
+            this.Id = id ?? Guid.NewGuid().ToString();
+            this.DateCreated = DateTime.Now;
+            this.Coins = 1000d;
+        }
+
+        [Key]
+        public string Id { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public DateTime DateCreated { get; private set; }
+        public double Coins { get; private set; }
+
+
+
+    }
+}
