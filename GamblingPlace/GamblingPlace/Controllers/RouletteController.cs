37c600de5abb4b7749864767b97ffda759b035fc
deivid7007 <7deividmladenov007@gmail.com>
2018-03-06 16:36:51 +0200

    Fixed session bug in the roulette page

--- GamblingPlace/GamblingPlace/Controllers/RouletteController.cs
+++ GamblingPlace/GamblingPlace/Controllers/RouletteController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GamblingPlace.Extensions;
+using GP.DB;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GamblingPlace.Controllers
 {
     public class RouletteController : Controller
     {
+        private string _userId;
+        private string _email;
+        private double _coins;
+        private GPDbContext _ctx = new GPDbContext();
+
         public IActionResult Index()
         {
             return View();
@@ -17,7 +24,26 @@ namespace GamblingPlace.Controllers
         [HttpGet]
         public IActionResult Roulette()
         {
+            _userId = HttpContext.Session.GetObjectFromJson<string>("UserId");
+            _email = HttpContext.Session.GetObjectFromJson<string>("Email");
+            if (_userId != null)
+            {
+                HttpContext.Session.SetObjectAsJson<double>("Coins", 0);
+                double coins = UpdateCoins(_userId);
+                HttpContext.Session.SetObjectAsJson<double>("Coins", coins);
+            }
+            _coins = HttpContext.Session.GetObjectFromJson<double>("Coins");
+            ViewData["UserId"] = _userId;
+            ViewData["Email"] = _email;
+            ViewData["Coins"] = _coins;
+
             return View();
+        }
+
+        public double UpdateCoins(string _userId)
+        {
+            var user = _ctx.Users.FirstOrDefault(u => u.Id == _userId);
+            return user.Coins;
         }
 
 
